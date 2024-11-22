12098d885949919f2adb111b6c2dd28f1dd175dd
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-22 21:06:32 +0100

    iter

--- sklearn_compat/utils/_indexing.py
+++ sklearn_compat/utils/_indexing.py
@@ -1,6 +1,6 @@
 from ._version import parse_version, sklearn_version
 
 if sklearn_version < parse_version("1.5"):
-    from sklearn.utils import _get_column_indices # type: ignore[no-redef]
+    from sklearn.utils import _get_column_indices
 else:
-    from sklearn.utils._indexing import _get_column_indices  # type: ignore[no-redef]
+    from sklearn.utils._indexing import _get_column_indices
--- sklearn_compat/utils/_user_interface.py
+++ sklearn_compat/utils/_user_interface.py
@@ -3,4 +3,4 @@ from ._version import parse_version, sklearn_version
 if sklearn_version < parse_version("1.5"):
     from sklearn.utils import _print_elapsed_time
 else:
-    from sklearn.utils._user_interface import _print_elapsed_time  # type: ignore[no-redef]
+    from sklearn.utils._user_interface import _print_elapsed_time
