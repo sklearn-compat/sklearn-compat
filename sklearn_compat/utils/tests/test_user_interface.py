9a151a13faf13bb1351ee40fce57cbc3f19347a0
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-22 19:02:05 +0100

    iter

---
+++ sklearn_compat/utils/tests/test_user_interface.py
@@ -0,0 +1,20 @@
+import time
+
+from sklearn_compat.utils._user_interface import _print_elapsed_time
+
+
+def test_print_elapsed_time():
+    """Check that we can import `_print_elapsed_time` from the right module.
+
+    This change has been done in scikit-learn 1.5.
+    """
+    from io import StringIO
+    import sys
+
+    stdout = StringIO()
+    sys.stdout = stdout
+    with _print_elapsed_time("sklearn_compat", "testing"):
+        time.sleep(0.1)
+    sys.stdout = sys.__stdout__
+    output = stdout.getvalue()
+    assert output.startswith("[sklearn_compat] .....")
