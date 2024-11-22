a5977ef595a77b1d0bc93508b09e462e63a224ed
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-22 18:46:54 +0100

    version module

--- sklearn_compat/utils/_indexing.py
+++ sklearn_compat/utils/_indexing.py
@@ -1,7 +1,4 @@
-import sklearn
-from sklearn.utils.fixes import parse_version
-
-sklearn_version = parse_version(parse_version(sklearn.__version__).base_version)
+from ._version import parse_version, sklearn_version
 
 if sklearn_version < parse_version("1.5"):
     from sklearn.utils import _get_column_indices
