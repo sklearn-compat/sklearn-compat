64451b4bb512d063ca0e0854c7e8e1f762a94bcb
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-22 17:42:22 +0100

    more doc

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 ## Ease multi-version support for scikit-learn compatible library
 
+[![SPEC 0 — Minimum Supported Dependencies](https://img.shields.io/badge/SPEC-0-green?labelColor=%23004811&color=%235CA038)](https://scientific-python.org/specs/spec-0000/)
+
 `sklearn_compat` is a small Python package that allows you to support new
 scikit-learn features with older versions of scikit-learn.
+
+The aim is to support a range of scikit-learn versions as specified in the
+[SPEC0](https://scientific-python.org/specs/spec-0000/). It means that you will find
+utility to support the last 4 released version of scikit-learn.
+
+## How to adapt your scikit-learn code
+
+### Upgrading to scikit-learn 1.6
+
+#### `validate_data`
+
+Your previous code could have looked like this:
+
+```python
+class MyEstimator(BaseEstimator):
+    def fit(self, X, y=None):
+        X = self._validate_data(X, force_all_finite=True)
+        return self
+```
+
+There is two major changes in scikit-learn 1.6:
+
+- `validate_data` has been moved to `sklearn.utils.validation`.
+- `force_all_finite` is deprecated in favor of the `ensure_all_finite` parameter.
+
+You can now use the following code for backward compatibility:
+
+```python
+from sklearn_compat.utils.validation import validate_data
+
+class MyEstimator(BaseEstimator):
+    def fit(self, X, y=None):
+        X = validate_data(self, X=X, ensure_all_finite=True)
+        return self
+```
--- sklearn_compat/utils/tests/test_validation.py
+++ sklearn_compat/utils/tests/test_validation.py
@@ -1,11 +1,21 @@
 import numpy as np
+import pytest
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from sklearn_compat.utils.validation import validate_data
+from sklearn_compat.utils.validation import (
+    validate_data,
+    _check_n_features,
+    _check_feature_names,
+)
+
 
 def test_validate_data():
+    """Check the behaviour of `validate_data`.
 
-    class MyEstimator(TransformerMixin,BaseEstimator):
+    This change has been introduced in scikit-learn 1.6.
+    """
+
+    class MyEstimator(TransformerMixin, BaseEstimator):
 
         def fit(self, X, y=None):
             X = validate_data(self, X=X, y=y)
@@ -18,3 +28,38 @@ def test_validate_data():
     X = [[1, 2, 3, 4]]
     est = MyEstimator()
     assert isinstance(est.fit_transform(X), np.ndarray)
+
+
+def test_check_n_features():
+    """Check the behaviour of `_check_n_features`.
+
+    This change has been introduced in scikit-learn 1.6.
+    """
+
+    class MyEstimator(TransformerMixin, BaseEstimator):
+
+        def fit(self, X, y=None):
+            X = _check_n_features(self, X, reset=True)
+            return self
+
+    X = [[1, 2, 3, 4]]
+    est = MyEstimator().fit(X)
+    assert est.n_features_in_ == 4
+
+
+def test_check_feature_names():
+    """Check the behaviour of `_check_feature_names`.
+
+    This change has been introduced in scikit-learn 1.6.
+    """
+    pd = pytest.importorskip("pandas")
+
+    class MyEstimator(TransformerMixin, BaseEstimator):
+
+        def fit(self, X, y=None):
+            X = _check_feature_names(self, X, reset=True)
+            return self
+
+    X = pd.DataFrame([[1, 2, 3, 4]], columns=["a", "b", "c", "d"])
+    est = MyEstimator().fit(X)
+    np.testing.assert_array_equal(est.feature_names_in_, ["a", "b", "c", "d"])
