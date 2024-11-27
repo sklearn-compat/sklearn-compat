411522edbd80eb49c1a1e4c8158553b9c6d617a5
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-27 19:04:23 +0100

    wip

--- sklearn_compat/utils/_indexing.py
+++ sklearn_compat/utils/_indexing.py
@@ -1,6 +1,19 @@
 from ._version import parse_version, sklearn_version
 
 if sklearn_version < parse_version("1.5"):
+    from sklearn.utils import _determine_key_type  # noqa: F401
+    from sklearn.utils import _safe_indexing  # noqa: F401
+    from sklearn.utils import _safe_assign  # noqa: F401
     from sklearn.utils import _get_column_indices  # noqa: F401
+    from sklearn.utils import _get_column_indices_interchange  # noqa: F401
+    from sklearn.utils import resample  # noqa: F401
+    from sklearn.utils import shuffle  # noqa: F401
+
 else:
+    from sklearn.utils._indexing import _determine_key_type  # noqa: F401
+    from sklearn.utils._indexing import _safe_indexing  # noqa: F401
+    from sklearn.utils._indexing import _safe_assign  # noqa: F401
     from sklearn.utils._indexing import _get_column_indices  # noqa: F401
+    from sklearn.utils._indexing import _get_column_indices_interchange  # noqa: F401
+    from sklearn.utils._indexing import resample  # noqa: F401
+    from sklearn.utils._indexing import shuffle  # noqa: F401
--- sklearn_compat/utils/tests/test_indexing.py
+++ sklearn_compat/utils/tests/test_indexing.py
@@ -1,9 +1,25 @@
 import pytest
 import numpy as np
 
-from sklearn_compat.utils._indexing import _get_column_indices
+from sklearn_compat.utils._indexing import (
+    resample,
+    shuffle,
+    _determine_key_type,
+    _safe_indexing,
+    _get_column_indices,
+)
 
-def test_get_column_indices():
+
+def test__determine_key_type():
+    assert _determine_key_type(np.arange(10)) == "int"
+
+
+def test__safe_indexing():
+    array = np.arange(10).reshape(2, 5)
+    np.testing.assert_allclose(_safe_indexing(array, 1, axis=1), array[:, 1])
+
+
+def test__get_column_indices():
     """Check that we can import `_get_column_indices` from the right module.
 
     This change has been done in scikit-learn 1.5.
@@ -11,3 +27,15 @@ def test_get_column_indices():
     pd = pytest.importorskip("pandas")
     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     assert np.array_equal(_get_column_indices(df, key="b"), [1])
+
+
+def test_resample():
+    array = np.arange(10)
+    resampled_array = resample(array, n_samples=20, replace=True, random_state=0)
+    assert len(resampled_array) == 20
+
+
+def test_shuffle():
+    array = np.arange(10)
+    shuffled_array = shuffle(array, random_state=0)
+    assert len(shuffled_array) == len(array)
