18eef7e60f82998a88aea7633fa3aef0f663092b
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-27 16:42:13 +0100

    forget import

--- sklearn_compat/utils/_optional_dependencies.py
+++ sklearn_compat/utils/_optional_dependencies.py
@@ -4,4 +4,7 @@ if sklearn_version < parse_version("1.5"):
     from sklearn.utils import check_matplotlib_support  # noqa: F401
     from sklearn.utils import check_pandas_support  # noqa: F401
 else:
+    from sklearn.utils._optional_dependencies import (  # noqa: F401
+        check_matplotlib_support,
+    )
     from sklearn.utils._optional_dependencies import check_pandas_support  # noqa: F401
