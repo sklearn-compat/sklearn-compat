11785bee26e71b2a5c4a1105bbe6bd5efac40d27
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-28 14:43:16 +0100

    add more fixes

--- src/sklearn_compat/utils/tests/test_fixes.py
+++ src/sklearn_compat/utils/tests/test_fixes.py
@@ -1,5 +1,16 @@
-from sklearn_compat.utils.fixes import _in_unstable_openblas_configuration
+from sklearn_compat.utils.fixes import (
+    _in_unstable_openblas_configuration,
+    _IS_WASM,
+    _IS_32BIT,
+)
 
 
 def test__in_unstable_openblas_configuration():
     _in_unstable_openblas_configuration()
+
+
+def test__IS_WASM():
+    assert not _IS_WASM
+
+def test__IS_32BIT():
+    assert not _IS_32BIT
