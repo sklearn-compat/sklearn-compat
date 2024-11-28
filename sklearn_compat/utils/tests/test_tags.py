e087cb20e0436278d3da5924867f663c73b1b3f5
adrinjalali <adrin.jalali@gmail.com>
2024-11-28 11:47:11 +0100

    tests

---
+++ sklearn_compat/utils/tests/test_tags.py
@@ -0,0 +1,18 @@
+
+from sklearn.base import BaseEstimator
+from sklearn_compat.utils._tags import (
+    get_tags,
+)
+
+def test_get_tags():
+    class MyEstimator(BaseEstimator):
+        def _more_tags(self):
+            return {"requires_fit": False}
+
+        def __sklearn_tags__(self):
+            tags = super().__sklearn_tags__()
+            tags.requires_fit = False
+            return tags
+
+    tags = get_tags(MyEstimator())
+    assert not tags.requires_fit
