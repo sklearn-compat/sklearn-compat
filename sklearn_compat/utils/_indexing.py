e9ec458eb9917c7d59686a6247587908c699fbae
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-28 11:10:43 +0100

    not exposing _get_column_indices_interechange

--- sklearn_compat/utils/_indexing.py
+++ sklearn_compat/utils/_indexing.py
@@ -5,7 +5,6 @@ if sklearn_version < parse_version("1.5"):
     from sklearn.utils import _safe_indexing  # noqa: F401
     from sklearn.utils import _safe_assign  # noqa: F401
     from sklearn.utils import _get_column_indices  # noqa: F401
-    from sklearn.utils import _get_column_indices_interchange  # noqa: F401
     from sklearn.utils import resample  # noqa: F401
     from sklearn.utils import shuffle  # noqa: F401
 
@@ -14,6 +13,5 @@ else:
     from sklearn.utils._indexing import _safe_indexing  # noqa: F401
     from sklearn.utils._indexing import _safe_assign  # noqa: F401
     from sklearn.utils._indexing import _get_column_indices  # noqa: F401
-    from sklearn.utils._indexing import _get_column_indices_interchange  # noqa: F401
     from sklearn.utils._indexing import resample  # noqa: F401
     from sklearn.utils._indexing import shuffle  # noqa: F401
