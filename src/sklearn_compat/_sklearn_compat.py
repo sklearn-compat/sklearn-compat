7c4d957ee9398f6c6919b79f612291282c0919f6
Guillaume Lemaitre <guillaume@probabl.ai>
2024-11-30 13:38:52 +0100

    ups

--- src/sklearn_compat/_sklearn_compat.py
+++ src/sklearn_compat/_sklearn_compat.py
@@ -128,29 +128,7 @@ def _to_new_tags(old_tags, estimator=None):
     )
 
 
-if sklearn_version >= parse_version("1.3"):
-    # parameter validation
-    class ParamsValidationMixin:
-        """Mixin class to validate parameters."""
-
-        def _validate_params(self):
-            """Validate types and values of constructor parameters.
-
-            The expected type and values must be defined in the `_parameter_constraints`
-            class attribute, which is a dictionary `param_name: list of constraints`. See
-            the docstring of `validate_parameter_constraints` for a description of the
-            accepted constraints.
-            """
-            if hasattr(self, "_parameter_constraints"):
-                validate_parameter_constraints(
-                    self._parameter_constraints,
-                    self.get_params(deep=False),
-                    caller_name=self.__class__.__name__,
-                )
-
-        from sklearn.base import _fit_context
-
-else:
+if sklearn_version < parse_version("1.3"):
     # parameter validation
     class ParamsValidationMixin:
         """Mixin class to validate parameters.
@@ -200,6 +178,27 @@ else:
             return wrapper
 
         return decorator
+else:
+    # parameter validation
+    class ParamsValidationMixin:
+        """Mixin class to validate parameters."""
+
+        def _validate_params(self):
+            """Validate types and values of constructor parameters.
+
+            The expected type and values must be defined in the `_parameter_constraints`
+            class attribute, which is a dictionary `param_name: list of constraints`. See
+            the docstring of `validate_parameter_constraints` for a description of the
+            accepted constraints.
+            """
+            if hasattr(self, "_parameter_constraints"):
+                validate_parameter_constraints(
+                    self._parameter_constraints,
+                    self.get_params(deep=False),
+                    caller_name=self.__class__.__name__,
+                )
+
+    from sklearn.base import _fit_context  # noqa: F401
 
 
 ########################################################################################
